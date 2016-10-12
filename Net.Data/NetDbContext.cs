424390a347fd4fafe55de9aa77e048312999d95d
PC\Admin <thangtq92@gmail.com>
2016-10-12 07:12:20 +0700

    Commit 12-07

--- Net.Data/NetDbContext.cs
+++ Net.Data/NetDbContext.cs
@@ -1,9 +1,34 @@
-﻿using System.Data.Entity;
+﻿using Net.Model.Models;
+using System.Data.Entity;
 
 namespace Net.Data
 {
     public class NetDbContext : DbContext
     {
+        public NetDbContext() : base("NetConnection")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
 
+        public DbSet<Footer> Footers { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuGroup> MenuGroups { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategorys { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<ProductTag> ProductTag { get; set; }
+        public DbSet<Slide> Slides { get; set; }
+        public DbSet<SupportOnline> SupportOnlines { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<VisitorStatistic> VisitorStatistics { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            
+        }
     }
 }
