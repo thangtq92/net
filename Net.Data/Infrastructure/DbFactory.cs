b897d7aac302e273f450ff833c646eab7eb836a9
PC\Admin <thangtq92@gmail.com>
2016-10-13 20:45:09 +0700

    Bai 6 add infrastructure

---
+++ Net.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace Net.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private NetDbContext dbContext;
+
+        public NetDbContext Init()
+        {
+            return dbContext ?? (dbContext = new NetDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
---
+++ Net.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿namespace Net.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private NetDbContext dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public NetDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+}
